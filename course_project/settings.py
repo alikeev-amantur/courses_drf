0cf16e5dd6e3cae65babd4c95889422c27efc85f
amantur-alikeev <amanturalikeev@gmail.com>
2022-09-17 14:49:49 +0600

    'heroku'

--- course_project/settings.py
+++ course_project/settings.py
@@ -126,3 +126,4 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
